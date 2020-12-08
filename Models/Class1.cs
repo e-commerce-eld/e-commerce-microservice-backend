c5168f4a7daea99d0808ee8d896104fdbd3ec239
Gabriela Alves da Gama <gabriela.gama@eldorado.org.br>
2020-12-08 11:13:47 -0300

    refactor: Deleted unnecessary classes
    Classes that was created accidently

--- Models/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrdemExample.Models
-{
-    public class Category
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
